5c507675514c8fadaa8c7a4d0d36745df7c6e514
Michail Tsyganov <tsyganov.michail@yandex.ru>
2022-03-12 20:27:30 +0300

    Move label to label.py

--- prometheus_querybuilder/query.py
+++ prometheus_querybuilder/query.py
@@ -1,17 +1,4 @@
-from dataclasses import dataclass
-
-
-SUPPORTED_MATCH_OPERATORS = ("=", "!=", "=~", "!~")
-
-
-@dataclass(frozen=True)
-class Label:
-    name: str
-    value: str
-    match_operator: str = "="
-
-    def __str__(self) -> str:
-        return self.name + self.match_operator + f'"{self.value}"'
+from .label import SUPPORTED_MATCH_OPERATORS, Label
 
 
 class Query:
