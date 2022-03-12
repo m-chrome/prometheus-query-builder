2ce5c6003b1ea5d9348e722b1e0fb90f9590dc42
Michail Tsyganov <tsyganov.michail@yandex.ru>
2022-03-12 20:06:23 +0300

    Add label method and tests

--- prometheus_querybuilder/query.py
+++ prometheus_querybuilder/query.py
@@ -1,6 +1,32 @@
+from dataclasses import dataclass
+
+
+SUPPORTED_MATCH_OPERATORS = ("=", "!=", "=~", "!~")
+
+
+@dataclass(frozen=True)
+class Label:
+    key: str
+    value: str
+    match_operator: str = "="
+
+    def __str__(self) -> str:
+        return self.key + self.match_operator + f'"{self.value}"'
+
+
 class Query:
     def __init__(self, metric: str):
         self.metric = metric
+        self.labels = set()
 
     def __str__(self) -> str:
-        return self.metric
+        query = self.metric
+        if self.labels:
+            # sort labels just because
+            query += "{" + ",".join(sorted(str(label) for label in self.labels)) + "}"
+        return query
+
+    def add_label(self, key: str, value: str, match_operator: str = "="):
+        if match_operator not in SUPPORTED_MATCH_OPERATORS:
+            raise ValueError("Wrong match operator")
+        self.labels.add(Label(key, value, match_operator))
--- tests/test_prometheus_querybuilder.py
+++ tests/test_prometheus_querybuilder.py
@@ -1,5 +1,39 @@
 from prometheus_querybuilder import __version__
+from prometheus_querybuilder.query import Query
+
+import pytest
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_query():
+    """ Test query with only metric name """
+    query = Query("http_requests_total")
+    assert str(query) == "http_requests_total"
+
+
+def test_query_with_label():
+    query = Query("http_requests_total")
+    query.add_label("environment", "production")
+    assert str(query) == 'http_requests_total{environment="production"}'
+
+
+def test_query_with_label_operators():
+    query = Query("http_requests_total")
+    query.add_label("environment", "production", "!=")
+    assert str(query) == 'http_requests_total{environment!="production"}'
+
+
+def test_query_with_unsupported_operator():
+    query = Query("http_requests_total")
+    with pytest.raises(ValueError):
+        query.add_label("environment", "production", "!===")
+
+
+def test_query_with_labels():
+    query = Query("http_requests_total")
+    query.add_label("environment", "production")
+    query.add_label("method", "GET")
+    assert str(query) == 'http_requests_total{environment="production",method="GET"}'
